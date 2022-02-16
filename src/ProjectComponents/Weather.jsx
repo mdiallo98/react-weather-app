31aade4d3202f579faa18073b7345061ce7acfd3
mdiallo98 <mamadou1237@gmail.com>
2022-02-16 04:15:03 -0500

    input onchange and value

--- src/ProjectComponents/Weather.jsx
+++ src/ProjectComponents/Weather.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function Weather({ weather }) {
+function Weather({ weather, SetSearch, searchData }) {
   console.log(weather, 'this is the data');
 
-  return <div></div>;
+  const getLocation = (event) => {
+    SetSearch(event.target.value);
+  };
+  const ResetInput = () => {
+    searchData('');
+  };
+
+  return (
+    <div>
+      <input type="text" value={searchData} onChange={getLocation()} />
+      <button>Search</button>
+    </div>
+  );
 }
 
 export default Weather;
--- src/ProjectComponents/WeatherContainer.jsx
+++ src/ProjectComponents/WeatherContainer.jsx
@@ -3,9 +3,11 @@ import Axios from 'axios';
 import Weather from './Weather';
 function WeatherContainer() {
   const [weather, Getweather] = useState();
+  const [searchData, SetSearch] = useState('');
   const apikey = '0e70b0ad034bbdb765793eff6eec2313';
   const workingKey =
     'http://api.weatherapi.com/v1/current.json?key=28ca387252884dfbbf7102312221502&q=New York &aqi=no';
+
   useEffect(() => {
     fetch(
       `  http://api.weatherapi.com/v1/current.json?key=28ca387252884dfbbf7102312221502&q=New York &aqi=no
@@ -19,7 +21,11 @@ function WeatherContainer() {
   }, []);
   return (
     <div>
-      <Weather weather={weather} />
+      <Weather
+        weather={weather}
+        searchData={searchData}
+        SetSearch={SetSearch}
+      />
     </div>
   );
 }
