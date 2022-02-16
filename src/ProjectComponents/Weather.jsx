3bb5a3fb0a0a9543c265b8e1b3899895a1f982f8
mdiallo98 <mamadou1237@gmail.com>
2022-02-16 04:08:29 -0500

    making input and search hooks

---
+++ src/ProjectComponents/Weather.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Weather({ weather }) {
+  console.log(weather, 'this is the data');
+
+  return <div></div>;
+}
+
+export default Weather;
---
+++ src/ProjectComponents/WeatherContainer.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Weather from './Weather';
+function WeatherContainer() {
+  const [weather, Getweather] = useState();
+  const apikey = '0e70b0ad034bbdb765793eff6eec2313';
+  const workingKey =
+    'http://api.weatherapi.com/v1/current.json?key=28ca387252884dfbbf7102312221502&q=New York &aqi=no';
+  useEffect(() => {
+    fetch(
+      `  http://api.weatherapi.com/v1/current.json?key=28ca387252884dfbbf7102312221502&q=New York &aqi=no
+        `
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        Getweather(data);
+        console.log(data, 'mydata');
+      });
+  }, []);
+  return (
+    <div>
+      <Weather weather={weather} />
+    </div>
+  );
+}
+
+export default WeatherContainer;
